f6d4285fa03adb683997c7dd1fdbe89ba7fdda5d
kimkangtae <tt997768@gmail.com>
2024-01-09 23:17:37 +0900

    Feat: router 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:todolist/ui/main_screen.dart';
+import 'package:todolist/di/di_setup.dart';
+import 'package:todolist/routes.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'data/hive/todo_hive_model.dart';
 
 late Box<Todo> todos;
 
 void main() async {
+  diSetup();
   await Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
   todos = await Hive.openBox<Todo>('todolist.db');
@@ -19,13 +21,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
     );
   }
 }
