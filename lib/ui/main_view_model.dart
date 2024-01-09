ab234aa5594daeab9817a291b6c9d6d4a4ffb027
kimkangtae <tt997768@gmail.com>
2024-01-09 18:23:03 +0900

    Feat: debounce 추가

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:todolist/data/repository/todo_repository.dart';
 
@@ -9,4 +11,22 @@ class MainViewModel extends ChangeNotifier {
   }) : _repository = repository;
 
 
+
+
+}
+
+
+
+class Debounce {
+  final Duration duration;
+  Timer? _timer;
+
+  Debounce(this.duration);
+
+  void onEvent(void Function() callback) {
+    if (_timer?.isActive ?? false) {
+      _timer?.cancel();
+    }
+    _timer = Timer(duration, () => callback());
+  }
 }
