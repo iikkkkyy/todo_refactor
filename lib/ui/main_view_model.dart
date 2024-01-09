dce51fa5367eef83ad39a702e444ec3820e8b32d
kimkangtae <tt997768@gmail.com>
2024-01-09 23:18:12 +0900

    Feat: 멀티서치 기능 추가

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -6,21 +6,31 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:todolist/data/model/todo_model.dart';
 import 'package:todolist/data/repository/todo_repository.dart';
 
+import '../data/repository/todo_repository_impl.dart';
+
 class MainViewModel extends ChangeNotifier {
   final ToDoRepository _repository;
 
   DateTime _focusedDay = DateTime.now().toUtc();
-  DateTime? _selectedDay;
+
+  // DateTime? _selectedDay;
   List<Event>? _selectedEvents;
+
   //TODO 뭔가 이상함
-  LinkedHashMap<DateTime, List<Event>> _events = LinkedHashMap<DateTime, List<Event>>(); // 초기화
+  LinkedHashMap<DateTime, List<Event>> _events =
+      LinkedHashMap<DateTime, List<Event>>(); // 초기화
 
   //Main State 적용 실패... => getter 사용
   get focusedDay => _focusedDay;
-  get selectedDay => _selectedDay;
+
+  // get selectedDay => _selectedDay;
+
   get selectedEvents => _selectedEvents;
+
   get onDaySelected => _onDaySelected;
+
   get events => _events;
+
   get getEventsForDay => _getEventsForDay;
 
   final _debounce = Debounce(const Duration(milliseconds: 500));
@@ -32,17 +42,24 @@ class MainViewModel extends ChangeNotifier {
   }
 
   _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    if (!isSameDay(_selectedDay, selectedDay)) {
-      _selectedDay = selectedDay;
-      _focusedDay = focusedDay;
-      notifyListeners();
+    _focusedDay = focusedDay;
+    // Update values in a Set
+    if (selectedDays.contains(selectedDay)) {
+      selectedDays.remove(selectedDay);
+    } else {
+      selectedDays.add(selectedDay);
     }
-    _selectedEvents = _events[_selectedDay]??[];
     notifyListeners();
+    //TODO _selectedEvents 처리
+    // _selectedEvents.value = _getEventsForDays(_selectedDays);
   }
 
-  List<Event> _getEventsForDay(DateTime day) {
+  final Set<DateTime> selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
 
+  List<Event> _getEventsForDay(DateTime day) {
     return _events[day] ?? [];
   }
 
