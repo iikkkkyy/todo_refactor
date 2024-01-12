89d33300e881199178a48c5e7758840c0df6fac5
kimkangtae <tt997768@gmail.com>
2024-01-12 19:07:24 +0900

    Feat: 모델변경(+ id,date,isDone)

--- lib/data/model/todo_model.dart
+++ lib/data/model/todo_model.dart
@@ -1,11 +1,17 @@
 class Event {
+  final int id;
   final String title;
+  final String date;
+  final bool isDone;
 
 
   @override
   String toString() => title;
 
   const Event({
+    required this.id,
     required this.title,
+    required this.date,
+    required this.isDone,
   });
 }
