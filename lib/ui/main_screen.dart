6cb9430fcdb2425d43491b730fe4a8cb2e264805
gmkil11 <mkkil4451@gmail.com>
2024-01-14 21:31:06 +0900

    fix: Todo List Style 변경 완료

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -43,140 +43,166 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
-
+        backgroundColor: Color.fromRGBO(248, 248, 248, 100),
       ),
-      body: Column(
-        children: [
-          TableCalendar<Event>(
-            focusedDay: mainViewModel.focusedDay,
-            firstDay: DateTime(2010, 10, 16),
-            // 달력 전체의 시작 날짜
-            lastDay: DateTime(2030, 3, 14),
-            // 달력 전체의 마지막 날짜
-            calendarFormat: _calendarFormat,
-            eventLoader: mainViewModel.getEventsForDay,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            selectedDayPredicate: (day) =>
-                mainViewModel.selectedDays.contains(day),
-            onDaySelected: mainViewModel.onDaySelected,
+      body: Container(
+        decoration: const BoxDecoration(color: Color.fromRGBO(248, 248, 248, 100)),
+        child: Column(
+          children: [
+            TableCalendar<Event>(
+              focusedDay: mainViewModel.focusedDay,
+              firstDay: DateTime(2010, 10, 16),
+              // 달력 전체의 시작 날짜
+              lastDay: DateTime(2030, 3, 14),
+              // 달력 전체의 마지막 날짜
+              calendarFormat: _calendarFormat,
+              eventLoader: mainViewModel.getEventsForDay,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              selectedDayPredicate: (day) =>
+                  mainViewModel.selectedDays.contains(day),
+              onDaySelected: mainViewModel.onDaySelected,
 
-            // Month / 2 weeks / week 전환 기능
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              InkWell(
-                onTap: () {
-                  mainViewModel.resetSelectedEvents();
-                },
-                child: const Row(
-                  children: [
-                    Icon(
-                      Icons.restart_alt_rounded,
-                      color: Colors.black87,
-                      size: 20,
-                    ),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      '선택초기화',
-                      style: TextStyle(
-                          color: Colors.black87,
-                          decoration: TextDecoration.underline,
-                        fontSize: 12,
-                        decorationStyle: TextDecorationStyle.solid,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: mainViewModel.selectedEvents,
-              builder: (context, value, _) {
-                return Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Column(
+              // Month / 2 weeks / week 전환 기능
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  onTap: () {
+                    mainViewModel.resetSelectedEvents();
+                  },
+                  child: const Row(
                     children: [
-                      const Text("Todo List",),
+                      Icon(
+                        Icons.restart_alt_rounded,
+                        color: Colors.black87,
+                        size: 20,
+                      ),
                       SizedBox(
-                        width: screenWidth * 0.9,
-                        height: screenHeight * 0.25,
-
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: value.length,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                margin: const EdgeInsets.symmetric(
-                                  horizontal: 12.0,
-                                  vertical: 4.0,
-                                ),
-                                decoration: BoxDecoration(
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.circular(12.0),
-                                ),
-                                child: ListTile(
-                                  onTap: () async {
-                                    print('${value[index]} ${value[index].id}');
-                                    await mainViewModel.tapIsDone(value[index].id);
-                                  },
-                                  leading: value[index].isDone
-                                      ? const Icon(Icons.check_circle,
-                                          color: Colors.green)
-                                      : const Icon(Icons.check_circle_outline),
-                                  title: Text(
-                                    '${value[index]}',
-                                    style: TextStyle(
-                                        color: value[index].isDone
-                                            ? Colors.grey
-                                            : Colors.black),
-                                  ),
-                                  subtitle: Text('${value[index].date}',
-                                      style: TextStyle(
-                                          color: value[index].isDone
-                                              ? Colors.grey
-                                              : Colors.black)),
-                                  trailing: value[index].isDone
-                                      ? GestureDetector(
-                                          onTap: () async {
-                                            await mainViewModel
-                                                .deleteTodo(value[index].id);
-                                          },
-                                          child: const Icon(Icons.delete_forever),
-                                        )
-                                      : null,
-                                  // title: Text(mainViewModel.selectedEvents[index].toString()),
-                                ),
-                              );
-                            }),
+                        width: 4,
+                      ),
+                      Text(
+                        '선택초기화',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            decoration: TextDecoration.underline,
+                          fontSize: 12,
+                          decorationStyle: TextDecorationStyle.solid,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
                       ),
                     ],
                   ),
-                );
-              },
+                ),
+              ],
             ),
-          )
-        ],
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                valueListenable: mainViewModel.selectedEvents,
+                builder: (context, value, _) {
+                  return Container(
+                    height: screenHeight * 0.25,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                          child: Text("Todo List", style: TextStyle(fontSize: 20)),
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.9,
+                          height: screenHeight * 0.25,
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: value.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 10.0,
+                                    vertical: 4.0,
+                                  ),
+                                  child: ListTile(
+                                    contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+                                    onTap: () async {
+                                      print('${value[index]} ${value[index].id}');
+                                      await mainViewModel.tapIsDone(value[index].id);
+                                    },
+                                    leading: SizedBox(
+                                      width: 200,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                                            child: Checkbox(
+                                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                              value: value[index].isDone,
+                                              onChanged: (bool? newValue) async {
+                                                await mainViewModel.tapIsDone(value[index].id);
+                                              },
+                                              activeColor: const Color.fromRGBO(14, 176, 186, 1),
+                                            ),
+                                          ),
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text('${value[index]}',
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400,
+                                                  color: value[index].isDone ? Colors.grey : Colors.black,
+                                                ),
+                                              ),
+                                              Text('${value[index].date}',
+                                                style: TextStyle(
+                                                  fontSize: 11,
+                                                  color: value[index].isDone ? Colors.grey : Colors.grey,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    trailing: value[index].isDone
+                                        ? GestureDetector(
+
+                                      onTap: () async {
+                                        await mainViewModel.deleteTodo(value[index].id);
+                                      },
+                                      child: const Icon(Icons.delete_forever),
+                                    )
+                                        : null,
+                                  )
+                                );
+                              }),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
